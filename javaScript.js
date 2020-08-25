bf218ebcab813fef69eedaf8149bab30d0affc5d
Ishaan Gupta <15ishaangupta@gmail.com>
2020-08-25 21:45:19 +0530

    animation completed

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,5 @@
 window.addEventListener('mousemove', eyeball);
-
+document.querySelector('body').addEventListener('click', f);
 function eyeball(){
 
 	var eye = document.querySelectorAll('.eye');
@@ -8,9 +8,13 @@ function eyeball(){
 		let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
 		let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
 
-		let radian = Math.atan2(event.pageX - x, event.pageY - y);
+		let radian = Math.atan2(event.pageX-x, event.pageY-y);
 		let rot = (radian*(180 / Math.PI)*-1)+270;
 
 		eye.style.transform = "rotate("+rot+"deg)";
 	})
+}
+function f(){
+	console.log(event.pageX);
+	console.log(event.pageY);
 }
